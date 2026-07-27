9c15a797bd04e338112e6e21187674a876715231
Abdullah-Bin-Zubair <970rajaabdullah9089@gmail.com>
2026-07-27 11:29:46 +0500

    feat: add a new fuction to get rendom data

--- task.js
+++ task.js
@@ -8,46 +8,60 @@ for (let i = 1; i <= 20; i++) {
 }
 //step 2
 function getGrade(marks) {
-    if (marks >= 90) {
-        return "A";
-    } else if (marks >= 80) {
-        return "B";
-    } else if (marks >= 70) {
-        return "C";
-    } else if (marks >= 60) {
-        return "D";
-    } else {
-        return "F";
-    }
-    console.log(getGrade(59));
+  if (marks >= 90) {
+    return "A";
+  } else if (marks >= 80) {
+    return "B";
+  } else if (marks >= 70) {
+    return "C";
+  } else if (marks >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+console.log(getGrade(59));
 
+//step 3
 
-
-  //step 3
-
-
-  const students = [
-    { name: "Ali", marks: 85 },
-    { name: "Ahmed", marks: 92 },
-    { name: "Sara", marks: 76 },
-    { name: "Ayesha", marks: 64 },
-    { name: "Zain", marks: 55 }
-  ];
-    function getGrade(marks) {
-    if (marks >= 90) {
-        return "A";
-    } else if (marks >= 80) {
-        return "B";
-    } else if (marks >= 70) {
-        return "C";
-    } else if (marks >= 60) {
-        return "D";
-    } else {
-        return "F";
-    }
-    console.log(getGrade(59));
-  for (const student of students) {
-    console.log(`${student.name} got a grade of ${getGrade(student.marks)}`);
+const students = [
+  { name: "Ali", marks: 85 },
+  { name: "Ahmed", marks: 92 },
+  { name: "Sara", marks: 76 },
+  { name: "Ayesha", marks: 64 },
+  { name: "Zain", marks: 55 },
+];
+function getGrade(marks) {
+  if (marks >= 90) {
+    return "A";
+  } else if (marks >= 80) {
+    return "B";
+  } else if (marks >= 70) {
+    return "C";
+  } else if (marks >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+console.log(getGrade(59));
+for (const student of students) {
+  console.log(`${student.name} got a grade of ${getGrade(student.marks)}`);
 }
 
 //step 4
+
+function getRandomStudent(studentsData) {
+  let random = Math.round(Math.random() * students.length);
+  return studentsData[random];
+}
+const studentsData = [
+  { name: "Ali", marks: 85 },
+  { name: "Ahmed", marks: 92 },
+  { name: "Sara", marks: 76 },
+  { name: "Ayesha", marks: 64 },
+  { name: "Zain", marks: 55 },
+];
+let randomStudent = getRandomStudent(students);
+console.log(randomStudent);
+console.log(`${randomStudent.name} number ${randomStudent.marks}`);
