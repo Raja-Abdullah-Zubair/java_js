25d54530fb6dea427900c6769946afd4b4b3a7ac
Abdullah-Bin-Zubair <970rajaabdullah9089@gmail.com>
2026-07-27 07:56:11 +0500

    feat: add a new fuction to check grade

--- task.js
+++ task.js
@@ -6,3 +6,18 @@ for (let i = 1; i <= 20; i++) {
     console.log(i);
   }
 }
+//step 2
+function getGrade(marks) {
+    if (marks >= 90) {
+        return "A";
+    } else if (marks >= 80) {
+        return "B";
+    } else if (marks >= 70) {
+        return "C";
+    } else if (marks >= 60) {
+        return "D";
+    } else {
+        return "F";
+    }
+    console.log(getGrade(59));
+  
