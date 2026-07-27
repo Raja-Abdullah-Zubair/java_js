5b34bb5a5af60b6bc26ce2dc6a0c432d4a45820b
Abdullah-Bin-Zubair <970rajaabdullah9089@gmail.com>
2026-07-27 08:02:27 +0500

    feat: add the name of students

--- task.js
+++ task.js
@@ -20,4 +20,34 @@ function getGrade(marks) {
         return "F";
     }
     console.log(getGrade(59));
-  
+
+
+
+  //step 3
+
+
+  const students = [
+    { name: "Ali", marks: 85 },
+    { name: "Ahmed", marks: 92 },
+    { name: "Sara", marks: 76 },
+    { name: "Ayesha", marks: 64 },
+    { name: "Zain", marks: 55 }
+  ];
+    function getGrade(marks) {
+    if (marks >= 90) {
+        return "A";
+    } else if (marks >= 80) {
+        return "B";
+    } else if (marks >= 70) {
+        return "C";
+    } else if (marks >= 60) {
+        return "D";
+    } else {
+        return "F";
+    }
+    console.log(getGrade(59));
+  for (const student of students) {
+    console.log(`${student.name} got a grade of ${getGrade(student.marks)}`);
+}
+
+//step 4
